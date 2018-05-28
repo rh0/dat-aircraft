8026ffb4f06df3aae6e87b8aea0c3ff0fd8e05a4
rho <ryan@theoleschool.com>
2018-05-28 16:10:45 -0500

    cleaning up the update method, removing debug

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const WebDB = require('@beaker/webdb')
 const assert = require('assert')
 const map = require('./lib/map')
 
-const webdb = new WebDB('flights', {
-    sparse: true,
-    sparseMetadata: true
-  })
+const webdb = new WebDB('flights')
 
 webdb.define('flights', {
   // validate required attributes before indexing
@@ -22,74 +19,69 @@ webdb.define('flights', {
 
 var allFlights = [];
 
-async function provision() {
-  await webdb.delete()
-  await webdb.open()
-
-  await webdb.indexArchive('dat://5263608e35f922e50999b5dd55a33055c324a913f707494d8e796586c09c7f24')
-
-  var fetchedFlights = await webdb.flights.toArray()
-  updateFlights(fetchedFlights)
+function updateFlight(flight) {
+  if(allFlights[flight.icao] !== undefined) {
+    allFlights[flight.icao] = {
+      oldLat: allFlights[flight.icao].lat,
+      oldLng: allFlights[flight.icao].lng,
+      lat: flight.lat,
+      lng: flight.lng,
+      callsign: flight.callsign
+    }
+  }
+  else {
+    allFlights[flight.icao] = {
+      lat: flight.lat,
+      lng: flight.lng,
+      callsign: flight.callsign
+    }
+  }
 }
 
-function updateFlights(fetchedFlights) {
+function updateAllFlights(fetchedFlights) {
   for(var flight in fetchedFlights) {
     flightUpdate = fetchedFlights[flight]
-    if(allFlights[flightUpdate.icao] !== undefined) {
-      allFlights[flightUpdate.icao] = {
-        oldLat: allFlights[flightUpdate.icao].lat,
-        oldLng: allFlights[flightUpdate.icao].lng,
-        lat: flightUpdate.lat,
-        lng: flightUpdate.lng,
-        callsign: flightUpdate.callsign
-      }
-    }
-    else {
-      allFlights[flightUpdate.icao] = {
-        lat: flightUpdate.lat,
-        lng: flightUpdate.lng,
-        callsign: flightUpdate.callsign
-      }
-    }
+    updateFlight(flightUpdate);
   }
   map.updateFlightPath(allFlights);
 }
 
 async function update() {
   fetchedFlights = await webdb.flights.toArray()
-  updateFlights(fetchedFlights)
+  updateAllFlights(fetchedFlights)
 }
 
-provision()
+(async function provision() {
+  await webdb.delete()
+  await webdb.open()
 
-webdb.on('open', () => {
-  console.log('Database opened.')
-})
+  await webdb.indexArchive('dat://5263608e35f922e50999b5dd55a33055c324a913f707494d8e796586c09c7f24')
 
-webdb.on('open-failed', (err) => {
-  console.log('Database failed to open', err)
-})
+  var fetchedFlights = await webdb.flights.toArray()
+  updateAllFlights(fetchedFlights)
+
+  // It may be better to implement flight updates this way rather than
+  // looping.  Need to work on the map update though to allow paths to
+  // without needing the previous coords.
+  /*webdb.flights.on('put-record', ({url, origin, indexedAt, record}) => {
+    console.log('record: ', record)
+    updateFlight(record)
+    map.updateFlightPath(allFlights);
+  })*/
+
+ // Flight is gone so remove from the array.
+  webdb.flights.on('del-record', ({url, origin, indexedAt}) => {
+    var icao = url.split('/').pop().split('.').shift()
+    allFlights.splice(icao, 1)
+  })
+
+})()
 
 webdb.on('indexes-updated', (url, version) => {
   update()
 })
 
 webdb.on('source-missing', (url) => {
-  console.log('WebDB could not find', url, ' ...continues searching')
+  console.log('WebDB could not find', url)
 })
 
-webdb.on('source-found', (url) => {
-  console.log('WebDB found:', url)
-})
-
-/*webdb.on('source-indexed', (url, version) => {
-  console.log('Source Indexed: Tables were updated for', url, 'at version', version)
-})*/
-
-webdb.on('indexes-reset', () => {
-  console.log('WebDB detected a change in schemas and reset all indexes')
-})
-
-webdb.on('source-error', (url, err) => {
-  console.log('An error has occured for ', url, ': ',err)
-})
